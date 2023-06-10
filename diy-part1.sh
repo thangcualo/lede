0f2321e778be8e4e8ec0d7ba9939c7e0d2b645b6
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2023-06-10 09:46:36 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+###!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -15,4 +16,10 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'src-git 3Ginfo https://github.com/4IceG/luci-app-3ginfo-lite.git' >>feeds.conf.default
+echo 'src-git Modem https://github.com/4IceG/luci-app-modemband.git' >>feeds.conf.default
+echo 'src-git SMS https://github.com/4IceG/luci-app-sms-tool.git' >>feeds.conf.default
+echo 'src-git atinout https://github.com/4IceG/luci-app-atinout-mod.git' >>feeds.conf.default
